e89adf05dd01f407ded2caced902802f9d586aef
yodin_2001 <yodin.2001@gmail.com>
2021-07-02 19:59:58 +0300

    Change baro logging mask

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -393,7 +393,7 @@ void Plane::startup_INS_ground(void)
 
     // read Baro pressure at ground
     //-----------------------------
-    barometer.set_log_baro_bit(MASK_LOG_IMU);
+    barometer.set_log_baro_bit(MASK_LOG_COMPASS);
     barometer.calibrate();
 
     if (airspeed.enabled()) {
